fc9d9aa06c4e5aca47379260cf21983cf5978388
olaniyan18 <olaniyanfeyikemi@gmail.com>
2024-12-23 19:52:21 +0100

    updated choose.jsx

--- src/components/Choose/Choose.jsx
+++ src/components/Choose/Choose.jsx
@@ -15,17 +15,17 @@ export default function Choose() {
       <div>
         <h2>Why You Should Choose Us:</h2>
         <ul>
-          <li>
+          <li className={styles.li}>
             <span>Comprehensive Wellness Solutions</span>: Tailored, evidence-
             based programs designed to enhance employee well-being and
             productivity.
           </li>
-          <li>
+          <li className={styles.li}>
             <span> Innovative Tools and Resources</span>: Real-time progress
             tracking and accessible resources to support meaningful workplace
             wellness.
           </li>
-          <li>
+          <li className={styles.standalone}>
             <span> Boost Engagement and Reduce Burnout</span>: Foster a
             healthier, more productive culture while empowering employees to
             thrive.
